ba447c9aee90d5961368541001eec2dd05b8ae52
LLstarla <loraleestarla@me.com>
2015-03-01 21:48:40 -0800

    minus owl

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-      $("#owl-demo").owlCarousel({
-        autoPlay: 3000,
-        items : 4,
-        itemsDesktop : [1199,3],
-        itemsDesktopSmall : [979,3]
-      });
-
-    });
